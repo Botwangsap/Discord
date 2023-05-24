4f644fc01d014284de2b194ef148f14bef7089db
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-05-24 12:47:59 +0700

    Update game-asahotak_ans.js

--- plugins/game-asahotak_ans.js
+++ plugins/game-asahotak_ans.js
@@ -1,17 +1,18 @@
-import similarity from 'similarity'
+const similarity = require('similarity')
 const threshold = 0.72
 let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*ao/i.test(m.quoted.contentText)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*ao/i.test(m.quoted.text)) return !0
     this.asahotak = this.asahotak ? this.asahotak : {}
     if (!(id in this.asahotak)) return m.reply('Soal itu telah berakhir')
     if (m.quoted.id == this.asahotak[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.asahotak[id][1]))
-        if (['.ao', 'Bantuan', ''].includes(m.text)) return !0
+        // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.asahotak[id][2]
-            await this.sendBut(m.chat, `*Benar!* +${this.asahotak[id][2]} XP`, 'By elyas', 'Asah Otak', '.asahotak', m)
+            global.db.data.users[m.sender].tiketcoin += 1
+            m.reply(`*Benar!*\n+${this.asahotak[id][2]} XP\n+1 TiketCoin`)
             clearTimeout(this.asahotak[id][3])
             delete this.asahotak[id]
         } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
@@ -21,4 +22,4 @@ handler.before = async function (m) {
 }
 handler.exp = 0
 
-export default handler
+module.exports = handler
