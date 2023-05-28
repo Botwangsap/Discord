dac318a6e2a3e28b0047a3de515c34a91c82bb25
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-05-29 06:15:22 +0700

    Update owner-listprem.js

--- plugins/owner-listprem.js
+++ plugins/owner-listprem.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, args }) => {
   let waktu = clockString(`${premTime - new Date() * 1} `)
   let sortedP = user.map(toNumber('premiumTime')).sort(sort('premiumTime'))
   let len = args[0] && args[0].length > 0 ? Math.min(100, Math.max(parseInt(args[0]), 10)) : Math.min(10, sortedP.length)
-  await conn.sendButton(m.chat, `${htki} *PREMIUM* ${htka}
+  await conn.reply(m.chat, `${htki} *PREMIUM* ${htka}
 ┌✦ *My Premium Time:*
 ┊• *Name:* ${conn.getName(m.sender)}
 ${prem ? `${clockString (premiumTime - new Date() * 1)}` : '┊• *PremiumTime:* Expired 🚫'}
@@ -17,7 +17,7 @@ ${prem ? `${clockString (premiumTime - new Date() * 1)}` : '┊• *PremiumTime:
 
 •·–––––––––––––––––––––·•
 ${sortedP.slice(0, len).map(({ jid, name, premiumTime, registered }, i) => `\n\n┌✦ ${registered ? name : conn.getName(jid)}\n┊• wa.me/${jid.split`@`[0]}\n${premiumTime > 0 ? `${clockString (premiumTime - new Date() * 1)}` : '┊ *EXPIRED 🚫*'}`).join`\n┗━═┅═━––––––๑`}
-┗━═┅═━––––––๑`.trim(), wm, null, [[`${prem ? '✦ Owner ✦': '✦ Buy Premium ✦'}`, `${prem ? '.owner nomor': '.premium'}`]], fkon)
+┗━═┅═━––––––๑`.trim(), wm, null, fkon)
 setTimeout(() => {
     if (db.data.chats[m.chat].deletemedia) conn.deleteMessage(m.chat, key)
   }, db.data.chats[m.chat].deletemediaTime)
