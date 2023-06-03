47a346dff04d8fe89db45cfd13a261b748a90513
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-06-04 01:14:30 +0700

    Update sfw.js

--- plugins/sfw.js
+++ plugins/sfw.js
@@ -1,18 +1,15 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, command, text, args }) => {
-  if (!args[0]) throw `Use example .pic neko`
-  let res = await fetch(`https://api.waifu.pics/sfw/${text}`)
+  if (!args[0]) throw `Use example .lewd neko`
+  let res = await fetch(`https://api.waifu.pics/nsfw/${text}`)
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', '2023 © Nao Bot V2', m)
+  conn.sendFile(m.chat, json.url, '', '2023 © Lisabotz-MD🍭🥰', m)
 }
-
-handler.tags = ['anime']
-handler.help = ['pic <category>']
-handler.command = /^(pic)$/i
-handler.premium = false
+handler.command = /^(lewd)$/i
+handler.premium = true
 handler.limit = true
 
 export default handler
