cc1e066225638a69a9965fbed63690d1d0d96980
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-06-03 10:53:10 +0700

    Update game-siapakahaku.js

--- plugins/game-siapakahaku.js
+++ plugins/game-siapakahaku.js
@@ -1,8 +1,11 @@
+import fetch from 'node-fetch'
 import { siapakahaku } from '@bochilteam/scraper'
 
 let timeout = 60000
 let poin = 4999
-let handler = async (m, { conn, usedPrefix }) => {
+let handler = async (m, { conn, command, usedPrefix }) => {
+let imgr = flaaa.getRandom()
+
     conn.siapakahaku = conn.siapakahaku ? conn.siapakahaku : {}
     let id = m.chat
     if (id in conn.siapakahaku) {
@@ -10,23 +13,23 @@ let handler = async (m, { conn, usedPrefix }) => {
         throw false
     }
     const json = await siapakahaku()
-    let caption = `
+    let caption = `*${command.toUpperCase()}*
 Siapakah aku? ${json.soal}
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}who untuk bantuan
+Ketik ${usedPrefix}hsi untuk bantuan
 Bonus: ${poin} XP
 `.trim()
     conn.siapakahaku[id] = [
-        await conn.reply(m.chat, caption, author, `${usedPrefix}who`, m),
+        await conn.sendFile(m.chat, `${imgr + command}`, null, caption,, m),
         json, poin,
         setTimeout(() => {
-            if (conn.siapakahaku[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, author, conn.siapakahaku[id][0])
+            if (conn.siapakahaku[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.siapakahaku[id][0])
             delete conn.siapakahaku[id]
         }, timeout)
     ]
 }
 handler.help = ['siapakahaku']
 handler.tags = ['game']
-handler.command = /^siapa(kah)?aku/i
+handler.command = /^siapakahaku/i
 
 export default handler
