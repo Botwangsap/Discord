7052d24506ef3e2d225ee75f1f437a1e1827cf9d
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-06-03 11:22:30 +0700

    Update downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,24 +1,17 @@
+//By: ZYKO MD
 import fetch from 'node-fetch'
 import fs from 'fs'
-let handler = async (m, { conn, usedPrefix, args, command, text }) => {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let name = await conn.getName(who)
-if (!args[0]) throw `Linknya?`
-  let res = await fetch(`http://saipulanuar.ga/api/download/fb?url=${args[0]}`)
-  let x = await res.json()
-  m.reply('Tunggu Sebentar...')
-  let cap = `Nih Kak Videonya >,<`
-  conn.sendFile(m.chat, x.result.hd, 'fb.mp4', cap, m)
+let handler = async(m, { conn, groupMetadata, usedPrefix, text, args, command }) => {
+//let imgr = flaaa2
+if (!args[0]) throw `MASUKAN URL FB`
+let f = await fetch(`https://saipulanuar.ga/api/download/fb?url=${args[0]}`)
+m.reply(md)
+let data = await f.json()
+let x = data.result
+//let x = await f.json()
+let cap = `DOWNLOADER FACEBOOK`
+conn.sendFile(m.chat, x.sd, 'facebook.mp4', cap, m)
 }
-
-handler.help = ['fb'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.limit = true
-
-handler.command = /^(fb(dl)?)$/i
-
+handler.help = ['facebook'].map(v => v + ' <url>')
+handler.command = /^((facebook|fb)(downloder|dl)?)$/i
 export default handler
-
-function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
-}
