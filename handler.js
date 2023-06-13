d9e9afd9854753562e944b744e9cdf5e853c6ccb
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-06-13 12:42:40 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -686,26 +686,38 @@ export async function participantsUpdate({ id, participants, action }) {
                 case 'add':
         case 'remove':
             if (chat.welcome) {
-                    let groupMetadata = await this.groupMetadata(id) || (conn.chats[id] || {}).metadata
-                    for (let user of participants) {
-                        let pp = 'https://i.ibb.co/sQTkHLD/ppkosong.png'
-                        let name = await this.getName(user)
-                        let gpname = await this.getName(id)
-                        let member = groupMetadata.participants.length
-                        pp: pp
-                        try {
-                            pp = await this.profilePictureUrl(user, 'image')
-                        } catch (e) {
-                        } finally {
-                            text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Welcome, @user!').replace('@subject', await this.getName(id)).replace('@desc', groupMetadata.desc ? String.fromCharCode(8206).repeat(4001) + groupMetadata.desc : '') :
-                                (chat.sBye || this.bye || conn.bye || 'Bye, @user!')).replace('@user', await this.getName(user))
-                            let wel = API(`https://api.lolhuman.xyz/api/base/welcome?apikey=BrunoSobrino&img1=https://i.postimg.cc/CM34YRFb/photo-2021-02-05-10-13-39.jpg&img2=https://i.postimg.cc/CM34YRFb/photo-2021-02-05-10-13-39.jpg&background=https://i.ibb.co/8B6Q84n/LTqHsfYS.jpg&username=${name}&member=${member}&groupname=${gpname}`)
-                            let lea = API(`https://api.lolhuman.xyz/api/base/leave?apikey=BrunoSobrino&img1=https://i.postimg.cc/CM34YRFb/photo-2021-02-05-10-13-39.jpg&img2=https://i.postimg.cc/CM34YRFb/photo-2021-02-05-10-13-39.jpg&background=https://i.ibb.co/8B6Q84n/LTqHsfYS.jpg&username=${name}&member=${member}&groupname=${gpname}`)
-                             this.sendFile(id, action === 'add' ? wel : lea, 'pp.jpg', text, null, false, { mentions: [user] })
-                        }
+                let groupMetadata = await this.groupMetadata(id) || (conn.chats[id] || {}).metadata
+                for (let user of participants) {
+                let welc = 'WELCOME'
+                    let outss = 'GOOD BYE'
+                    let pp = 'https://telegra.ph/file/2d06f0936842064f6b3bb.png'
+                    try {
+                        pp = await this.profilePictureUrl(user, 'image')
+                    } catch (e) {
+                    } finally {
+                        text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Welcome, @user').replace('@subject', await this.getName(id)).replace('@desc', groupMetadata.desc?.toString() || 'unknow') :
+                            (chat.sBye || this.bye || conn.bye || 'Bye @user')).replace(/@user/g, '@' + user.split`@`[0])
+let wel = API('popcat', '/welcomecard', {
+                                background: 'https://telegra.ph/file/4cc31db6304eb90149b88.jpg',
+                                text1: welc,
+                                text2: await this.getName(user),
+                                text3: 'MemberCount: ' + groupMetadata.participants.length, 
+                                avatar: pp,
+                            })
+                            let lea = API('popcat', '/welcomecard', {
+                                background: 'https://telegra.ph/file/4cc31db6304eb90149b88.jpg',
+                                text1: outss,
+                                text2: await this.getName(user),
+                                text3: 'MemberCount: ' + groupMetadata.participants.length,
+                                avatar: pp,
+                            })
+
+this.sendFile(id, pp, 'pp.jpg', text, null, false, { mentions: [user] })
+  
                     }
                 }
-                break 
+            }
+            break
  
         case 'promote':
             text = (chat.sPromote || this.spromote || conn.spromote || '@user ```is now Admin```')
