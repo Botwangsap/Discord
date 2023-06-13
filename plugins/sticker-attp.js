f537cee7ddf2e768c715f6b72ad5cd396813885b
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-06-13 12:30:06 +0700

    Update sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,5 +1,5 @@
 let handler = async(m, { conn, text, args, usedPrefix, command }) => {
-    if (!text) throw `*CONTOH*\n${usedPrefix + command} Nao Tomori`
+    if (!text) throw `*CONTOH*\n${usedPrefix + command} AnggaKz`
     let teks = encodeURI(text)
     if (command == 'attp') {
     conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp?apikey=${global.lolkey}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
