7fc04bfc6bb5c71766dae0005bfa589b47c10108
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-06-19 20:17:35 +0700

    Update game-susunkata_ans.js

--- plugins/game-susunkata_ans.js
+++ plugins/game-susunkata_ans.js
@@ -1,31 +1,25 @@
 import similarity from 'similarity'
 const threshold = 0.72
-export async function before(m) {
+let handler = m => m
+handler.before = async function (m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/Ketik.*hsus/i.test(m.quoted.text) || /.*hsus/i.test(m.text))
-        return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*suska/i.test(m.quoted.text)) return !0
     this.susunkata = this.susunkata ? this.susunkata : {}
-    if (!(id in this.susunkata))
-        return conn.reply(m.chat, 'Soal itu telah berakhir', m)
+    if (!(id in this.susunkata)) return m.reply('Soal itu telah berakhir')
     if (m.quoted.id == this.susunkata[id][0].id) {
-        let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
-        if (isSurrender) {
-            clearTimeout(this.susunkata[id][3])
-            delete this.susunkata[id]
-            return conn.reply(m.chat, '*Yah Menyerah :( !*', m)
-        }
         let json = JSON.parse(JSON.stringify(this.susunkata[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.susunkata[id][2]
-            conn.reply(m.chat, `*Benar!*\n+${this.susunkata[id][2]} XP`, m)
+            global.db.data.users[m.sender].money += this.susunkata[id][2]
+            global.db.data.users[m.sender].limit += 1
+            m.reply(`*Benar!*\n+${this.susunkata[id][2]} Money\n+1 Limit`)
             clearTimeout(this.susunkata[id][3])
             delete this.susunkata[id]
-        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold)
-            m.reply(`*Dikit Lagi!*`)
-        else
-            conn.reply(m.chat, `*Salah!*`, m)
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
+        else m.reply(`*Salah!*`)
     }
     return !0
 }
-export const exp = 0
+handler.exp = 0
+
+export default handler
