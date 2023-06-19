9461bc1f7dda2869e792378fef0a1a7fdb6a956c
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-06-19 20:16:22 +0700

    Update game-susunkata.js

--- plugins/game-susunkata.js
+++ plugins/game-susunkata.js
@@ -1,27 +1,29 @@
 import fetch from 'node-fetch'
-let timeout = 120000
-let poin = 4999
-let handler = async (m, { conn, command, usedPrefix }) => {
 
+let timeout = 120000
+let money = 4999
+let limit = 1
+let handler = async (m, { conn, usedPrefix }) => {
     conn.susunkata = conn.susunkata ? conn.susunkata : {}
     let id = m.chat
     if (id in conn.susunkata) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.susunkata[id][0])
+        conn.reply(m.chat, ' *Masih Ada Soal Yang Belu Terjawab* ', conn.susunkata[id][0])
         throw false
     }
     let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/susunkata.json')).json()
-  let json = src[Math.floor(Math.random() * src.length)]
-  let caption = `*${command.toUpperCase()}*
-  ${json.soal}
-  ${json.tipe}
+    let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `
+${json.soal}
 
+Tipe : ${json.tipe}
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}hsus untuk bantuan
-Bonus: ${poin} XP
-    `.trim()
+Ketik ${usedPrefix}suska untuk bantuan
+Bonus: ${money} Money
+Limit: ${limit} Limit
+`.trim()
     conn.susunkata[id] = [
-        await conn.sendFile(m.chat, null, caption, m),
-        json, poin,
+        await conn.reply(m.chat, caption, m),
+        json, money,
         setTimeout(() => {
             if (conn.susunkata[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.susunkata[id][0])
             delete conn.susunkata[id]
@@ -30,6 +32,8 @@ Bonus: ${poin} XP
 }
 handler.help = ['susunkata']
 handler.tags = ['game']
-handler.command = /^susunkata/i
+handler.command = /^susunkata|sskata/i
+handler.limit = true
+handler.group = true
 
 export default handler
