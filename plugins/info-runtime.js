79d93bd06f2c56e386f8bb8d9bec2f63f01fbf42
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-06-22 20:31:55 +0700

    Update info-runtime.js

--- plugins/info-runtime.js
+++ plugins/info-runtime.js
@@ -1,57 +1,30 @@
-
-
 import fs from 'fs'
 import fetch from 'node-fetch'
 import moment from 'moment-timezone'
+let handler = async (m, { conn, args, command }) => {
+	let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+    
+m.reply(`*Bot Telah Online*\n${muptime}`)
 
-const handler = async (m, { conn, args, command }) => {
-let _muptime
-if (process.send) {
-process.send('uptime')
-_muptime = await new Promise(resolve => {
-process.once('message', resolve)
-setTimeout(resolve, 1000)
-}) * 1000
 }
-let pp = hwaifu.getRandom()
-const muptime = clockString(_muptime)
-let greeting = `${ucapan()}`
-let pesan = `*${greeting} ${m.sender.split("@")[0]}*\n\n 🕰️ Bot Uptime: ${muptime}\n\nSilahkan ketik .menu untuk melihat menu bot.`
-
-conn.sendFile(m.chat, pp, 'eror.jpg', pesan, m)
-}
-
 handler.help = ['runtime']
 handler.tags = ['info']
 handler.command = ['runtime', 'rt']
 
 export default handler
 
-function ucapan() {
-const time = moment.tz('Asia/Jakarta').format('HH')
-let res = "Sudah Dini Hari Kok Belum Tidur Kak? 🥱"
-if (time >= 4) {
-res = "Pagi Lord 🌄"
-}
-if (time >= 10) {
-res = "Selamat Siang Kak ☀️"
-}
-if (time >= 15) {
-res = "Selamat Sore Kak 🌇"
-}
-if (time >= 18) {
-res = "Malam Kak 🌙"
-}
-return res
-}
-
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]
-}
-
 function clockString(ms) {
-let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, ' H ', m, ' M ', s, ' S '].map(v => v.toString().padStart(2, 0)).join('')
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [d, ' *Days ☀️* \n', h, ' *Hours 🕐*\n', m, ' *Minute ⏰*\n', s, ' *Second ⏱️*'].map(v => v.toString().padStart(2, 0)).join('')
 }
