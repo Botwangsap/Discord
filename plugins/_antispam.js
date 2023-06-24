9dc3b00d0c5f6007fcd1bdbe7d7912ad1aca063a
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-06-24 19:09:12 +0700

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -4,19 +4,19 @@ export async function all(m) {
     this.spam = this.spam ? this.spam : {}
     if (m.sender in this.spam) {
         this.spam[m.sender].count++
-        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
-            if (this.spam[m.sender].count > 10) {
+        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 5) {
+            if (this.spam[m.sender].count > 1) {
                 //global.db.data.users[m.sender].banned = true
                 m.reply('*Jangan Spam,jeda 5 detik sayang😋🥰😍!!*')
             }
-            this.spam[m.sender].count = 0
+            this.spam[m.sender].count = 1
             this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
         }
     }
     else
         this.spam[m.sender] = {
             jid: m.sender,
-            count: 0,
+            count: 1,
             lastspam: 5
         }
 }
