0d54a3868e66707f131ec356f6a6caa0bd7eae52
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-06-24 19:13:53 +0700

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -1,25 +1,23 @@
-let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
+const isToxic = /(anj[kg]|ajn[gk]|a?njin[gk]|bajingan|b[a]?[n]?gsa?t|ko?nto?l|me?me?[kq]|pe?pe?[kq]|meki|titi[t,d]|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|[kng]e?nto?[t,d]|jembut|bego|dajjal|janc[uo]k|pantek|puki?(mak)?|kimak|kampang|lonte|col[i,mek]|pelacur|henceut|nigga|fuck|dick|bitch|tits|bastard|asshole|a[su,w,yu])/i;
 
-export function before(m, { isBotAdmin }) {
-    if (m.isBaileys && m.fromMe) return !0
+export async function before(m, { isAdmin, isBotAdmin }) {
+    if (m.isBaileys && m.fromMe)
+        return !0
+    if (!m.isGroup) return !1
     let chat = global.db.data.chats[m.chat]
-    let user = global.db.data.users[m.sender]
-    let isBadword = badwordRegex.exec(m.text)
-    console.log(isBadword)
-
-    if (chat.antiBadword && isBadword) {
-        user.warning += 1
-        m.reply('Jangan Toxic ya!!\n' + `kamu memiliki ${user.warning} warning` + '\nUntuk mematikan ketik *.disable antibadword*')
-        if (user.warning >= 5) {
-            user.banned = false
-            user.warning = 0
-            if (m.isGroup) {
-                if (isBotAdmin) {
-                	this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
-                   //this.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
-                }
-            }
-        }
+    let bot = global.db.data.settings[this.user.jid] || {}
+    const isAntiToxic = isToxic.exec(m.text)
+    let hapus = m.key.participant
+    let bang = m.key.id
+    
+    if (chat.antiToxic && isAntiToxic) {
+	conn.sendFile(m.chat, 'https://bucin-livid.vercel.app/audio/toxic.mp3', "toxic.mp3", null, m, true, {
+		type: "audioMessage",
+		ptt: true,
+	})
+if (isBotAdmin && bot.restrict) {
+    return this.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
+        } else if (!bot.restrict) return m.reply('Semoga harimu suram!')
     }
     return !0
 }
