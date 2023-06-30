e9f7aebd1d3d8f97b725da26b1402b8d039095b5
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-06-30 09:57:28 +0700

    Update maker-loli.js

--- plugins/maker-loli.js
+++ plugins/maker-loli.js
@@ -1,15 +1,14 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
 let response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Parameter'
-  m.reply('Proses...')
-  let res = `https://ziy.herokuapp.com/api/maker/lolimaker?nama=${response[0]}&apikey=xZiyy`
-  conn.sendFile(m.chat, res, 'sadboy.jpg', `Sudah Jadi`, m, false)
+  if (!args[0]) throw 'Teksnya?'
+  let res = `https://oni-chan.my.id/api/canvas/loliGgo?name=${response[0]}&apikey=`
+  conn.sendFile(m.chat, res, 'yae.jpg', `Nih Kak`, m, false)
 }
-handler.help = ['logololi'].map(v => v + ' <text>')
+handler.help = ['loliggo']
 handler.tags = ['maker']
-handler.command = /^(logololi)$/i
-handler.register = true
+handler.command = /^(loliggo)$/i
+handler.register = false
 
 handler.limit = true
 
