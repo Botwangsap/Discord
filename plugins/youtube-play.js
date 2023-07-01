5ae11d31fb2e55eee21108ea9b8816d002bdd2d5
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-07-02 06:46:47 +0700

    Update youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -1,44 +1,30 @@
 import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
 import yts from 'yt-search'
 var handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `Use example\n${usedPrefix}${command} Dear Good`
-  await m.reply(wait)
+  if (!text) throw `Judulnya?`
+  m.reply(wait)
   let search = await yts(text)
   let vid = search.videos[Math.floor(Math.random() * search.videos.length)]
-  if (!search) throw 'Video Not Found, Try Another Title'
+  if (!search) throw 'Tidak Ditemukan'
   let { title, thumbnail, timestamp, views, ago, url } = vid
 
-  let captvid = `╭──── 〔 Y O U T U B E 〕 ─⬣
-⬡ Judul: ${title}
-⬡ Durasi: ${timestamp}
-⬡ Views: ${views}
-⬡ Upload: ${ago}
-⬡ Link: ${url}
-╰────────⬣`
-  conn.sendButton(m.chat, `╭──── 〔 Y O U T U B E 〕 ─⬣
-⬡ Judul: ${title}
-⬡ Durasi: ${timestamp}
-⬡ Views: ${views}
-⬡ Upload: ${ago}
-⬡ Link: ${url}
-╰────────⬣`, author.trim(), await( await conn.getFile(thumbnail)).data, ['📽VIDEO', `${usedPrefix}ytv ${url}`], false, { quoted: m, 'document': { 'url':'https://wa.me/6285923463470' },
-'mimetype': global.dpdf,
-'fileName': `Youtube Play`,
-'fileLength': 666666666666666,
-'pageCount': 666,contextInfo: { externalAdReply: { showAdAttribution: true,
-mediaType:  2,
-mediaUrl: `${url}`,
-title: `AUDIO SEDANG DIKIRIM...`,
-body: wm,
-sourceUrl: 'http://wa.me/6285923463470', thumbnail: await ( await conn.getFile(thumbnail)).data
-  }
- } 
-})
-  
-  //let buttons = [{ buttonText: { displayText: '📽VIDEO' }, buttonId: `${usedPrefix}ytv ${url} 360` }]
- //let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author, buttons }, { quoted: m })
+  let captvid = `╭──── 〔 Y O U T U B E 〕
+• Judul: ${title}
 
-  const yt = await await youtubedlv2(url).catch(async _ => await youtubedl(url)).catch(async _ => await youtubedlv3(url))
+• Durasi: ${timestamp}
+
+• Views: ${views}
+
+• Upload: ${ago}
+
+• Link: ${url}
+
+• Note: Untuk Video Ketik .ytv Masukan Link Atas
+
+• Contoh: .ytv ${url}
+╰────────⬣`
+conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid }, m)
+const yt = await await youtubedlv2(url).catch(async _ => await youtubedl(url)).catch(async _ => await youtubedlv3(url))
 const link = await yt.audio['128kbps'].download()
   let doc = { 
   audio: 
@@ -57,15 +43,13 @@ thumbnail: await(await conn.getFile(thumbnail)).data
   }
 
   return conn.sendMessage(m.chat, doc, { quoted: m })
-	// return conn.sendMessage(m.chat, { document: { url: link }, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, { quoted: m})
-	// return await conn.sendFile(m.chat, link, title + '.mp3', '', m, false, { asDocument: true })
 }
-handler.help = ['play'].map(v => v + ' <pencarian>')
-handler.tags = ['downloader', 'limitmenu']
+handler.help = ['play']
+handler.tags = ['downloader']
 handler.command = /^play?$/i
 
 handler.exp = 0
-handler.register = true
 handler.limit = true
+handler.register = false
 
 export default handler
