8859c0eebc640a6509730fcd31a6fcc891bbf71f
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-07-12 22:09:06 +0700

    Update info-tqto.js

--- plugins/info-tqto.js
+++ plugins/info-tqto.js
@@ -3,17 +3,12 @@ let handler = async (m, { conn }) => {
   let caption = `
 *「 Owner Website 」*
 
-My instagram -> https://instagram.com/anggatzi123
-My GroupBotz -> https://chat.whatsapp.com/Ly28IB8Fel4JbPS0qCUy2p
+follow My instagram -> https://instagram.com/anggatzi123
+official bot -> https://chat.whatsapp.com/Ly28IB8Fel4JbPS0qCUy2p
 Donasi DANA -> https://wa.me/p/9545254345500673/6281998425782
 
 ----------------------
-My instagram -> https://instagram.com/anggatzi123
 ----------------------
-
-My instagram -> https://instagram.com/anggatzi123
-My GroupBotz -> https://chat.whatsapp.com/Ly28IB8Fel4JbPS0qCUy2p
-Donasi DANA -> https://wa.me/p/9545254345500673/6281998425782
 `.trim()
   m.reply(caption)
 }
